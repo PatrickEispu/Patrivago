08ab2343fdd361a2db265181383ef1858a56d73d
Patrick Eispu Simões <patrickeispu.arc@gmail.com>
2024-07-15 16:15:56 -0300

    Primeiro commit do projeto SRH

---
+++ src/main/java/br/com/srh/Patrivago/PatrivagoApplication.java
@@ -0,0 +1,15 @@
+package br.com.srh.Patrivago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrivagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrivagoApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/br/com/srh/Patrivago/PatrivagoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.srh.Patrivago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrivagoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
